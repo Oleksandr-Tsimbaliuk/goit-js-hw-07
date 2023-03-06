c49482882d1cbb853e270cb738880109584f37c5
Oleksandr <115025828+g0d2illa@users.noreply.github.com>
2023-03-07 00:05:46 +0200

    Update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,35 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+function createGalleryItemsMarkup (galleryItems) {
+    return galleryItems.map((item) => {
+        const {preview, original, description} = item
+        return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}" />
+            </a>
+        </div>`
+        }).join(" ")
+}
+const galleryContainer = document.querySelector('.gallery')
+const gallaryMarkup = createGalleryItemsMarkup(galleryItems)
+galleryContainer.insertAdjacentHTML('beforeend', gallaryMarkup)
+
+galleryContainer.addEventListener('click', handlGalleryContainer)
+
+function handlGalleryContainer(event) {
+    event.preventDefault()
+    const largeImg = event.target.dataset.source
+    if (!event.target.classList.contains("gallery__image")) {
+        return
+    }
+    
+    basicLightbox.create(`
+		<img src="${largeImg}">
+	`).show()
+}
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
+function createGalleryItemsMarkup (galleryItems) {
+    return galleryItems.map((item) => {
+        const {preview, original, description} = item
+        return `
+        <li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}" />
+            </a>
+        </li>`
+        }).join(" ")
+}
+const galleryContainer = document.querySelector('.gallery')
+const gallaryMarkup = createGalleryItemsMarkup(galleryItems)
+galleryContainer.insertAdjacentHTML('beforeend', gallaryMarkup)
 console.log(galleryItems);
+
+const createLightBox = new SimpleLightbox('.gallery a', {captionsData: "alt", captionDelay: 250});
